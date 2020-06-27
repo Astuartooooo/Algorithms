64321d231ff8e85d68011c89a04c10ffe1f30391
Astuartooooo <990928xb@gmail.com>
2020-06-27 14:07:18 +0800

    added shell sort, a very good sorting algorithm

---
+++ sortingAlgorithms/Shell.java
@@ -0,0 +1,29 @@
+package sortingAlg;
+
+public class Shell {
+    public static void sort(int[] arr) {
+        int len= arr.length;
+        for (int gap = len / 2; gap > 0; gap = gap/ 2) {
+            for (int i = gap; i < len; i++) {
+                int j = i;
+                while ((j-gap)>=0 && arr[j-gap]>arr[j]){
+                    sortingAlg.Swap.arr(arr, j, j-gap);
+                    j = j-gap;
+                }
+            }
+        }
+    }
+
+    public static void sort(double[] arr) {
+        int len= arr.length;
+        for (int gap = len / 2; gap > 0; gap = gap/ 2) {
+            for (int i = gap; i < len; i++) {
+                int j = i;
+                while ((j-gap)>=0 && arr[j-gap]>arr[j]){
+                    sortingAlg.Swap.arr(arr, j, j-gap);
+                    j = j-gap;
+                }
+            }
+        }
+    }
+}
